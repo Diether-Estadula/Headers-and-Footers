0bb84890669a8f64584cbca5be4ebc55d959a309
Zappylicious <zappylicious001@gmail.com>
2025-07-23 13:41:45 +0800

    Initialized Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,27 @@ document.addEventListener('DOMContentLoaded', function () {
     closeBtn.addEventListener('click', () => {
       navbar.classList.remove('show');
     });
+
+    window.addEventListener('scroll', function () {
+      const header = document.getElementById('transparent-header');
+      if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+      }
+    });
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
 });
